382cf1964ec62451d73ea4a6fba1e90212042d5e
Ahmed Shaikh <72272879+shaikhahmed243@users.noreply.github.com>
2022-11-10 19:32:05 +0530

    Add files via upload

---
+++ Python programs.py/Chapter2.py/variable.py
@@ -0,0 +1,18 @@
+a = "ahmed"
+b = " abc"
+c = 354
+d = 45.5
+e = ''' Hi,I'm Ahmed'''
+
+#printing of variables
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+#logical oprator
+bol1 = True
+bol2 = False
+print("The value of bol1 and bol2 is",(bol1 and bol2))
+print("The value of bol1 or bol2 is",(bol1 or bol2))
