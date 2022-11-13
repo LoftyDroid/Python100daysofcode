4cb2dbbced2a6f51533ee8073d2b25d764023645
Ahmed Shaikh <72272879+LoftyDroid@users.noreply.github.com>
2022-11-13 21:58:52 +0530

    Add files via upload

---
+++ Chapter2.py/Practice_qs.py
@@ -0,0 +1,10 @@
+#Add to numers
+a = 56
+b = 58
+c = a+b
+print("the sum of a and b is",c)
+
+# Write a pyton program to find the remainder when a number is divided by Z
+a = 45
+b = 15
+print("The remainder of a and b is:",a%b)
